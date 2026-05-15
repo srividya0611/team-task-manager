09dcadf93916d18db44993276d2fcbf2f1614035
srividya0611 <srividyanimmala06@gmail.com>
2026-05-15 15:51:51 +0530

    Saved controller path fixes

--- server/src/controllers/projectControllers.js
+++ server/src/controllers/projectControllers.js
@@ -1,4 +1,4 @@
-const Project = require("../models/Project");
+const Project = require("../models/project");
 
 // Create Project
 const createProject = async (req, res) => {
--- server/src/controllers/taskControllers.js
+++ server/src/controllers/taskControllers.js
@@ -1,4 +1,4 @@
-const Task = require("../models/task.js");
+const Task = require("../models/task");
 
 // Create Task
 const createTask = async (req, res) => {
